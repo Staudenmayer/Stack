00870e35ce02bf593f609f853ea0ed74b0308485
Staudiz <s.staudenmayer@outlook.com>
2024-02-25 16:52:31 +0100

    Added Custom error handler

--- API/index.js
+++ API/index.js
@@ -13,9 +13,15 @@ const db = require('./utils/db');
 require('./utils/passport');
 
 const app = express();
+
+//Cors setup
 app.use(cors());
+
+//Body parser setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Session setup
 app.use(session({ 
     secret: process.env.SECRET || "secret",
     cookie: {
@@ -27,12 +33,23 @@ app.use(session({
     name: process.env.COOKIE_NAME || "session",
     store: new pgSession({pool: db, tableName: 'session_store'})
 }));
+
+//Passport setup
 app.use(passport.initialize());
 app.use(passport.session());
 
+//Swagger setup
 app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerJsdoc(require('./docs/options.json')), { explorer: true }));
+
+//Private and Public routes
 app.use('', require('./public/auth'));
 app.use('', auth, require('./private/user'));
+
+//Custom Error Handler
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).end();
+})
 
 app.listen(process.env.PORT || 3002, () => {
     console.log('Server running on http://localhost:' + process.env.PORT || 3002)
