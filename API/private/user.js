181adf79077855e1903f5438e145312d45eab114
Staudiz <s.staudenmayer@outlook.com>
2024-03-09 07:00:10 +0100

    Deleted Express API as it was replaced with h3

--- API/private/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require('express');
-const router = express.Router();
-
-router.get('/user', (req,res) => {
-    res.status(200).end(JSON.stringify(req.user));
-})
-
-module.exports = router;
--- API/utils/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const {ensureLoggedIn} = require('connect-ensure-login');
-
-module.exports = {
-  auth: function (req, res, next) {
-    if( req.user && req.user.csrf && req.user.csrf === req.headers['x-csrf-token']) (ensureLoggedIn())(req,res,next);
-    else return res.status(401).end();
-  }
-}
